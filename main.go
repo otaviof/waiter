711e7b18fc8db2ce7d95504f59cfbeb27fb02a2b
Otávio Fernandes <otaviof@gmail.com>
2021-06-21 08:30:43 +0200

    Improved long description.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// rootCmd primary cobra.Command instance, sub-commands will be attached to it.
 var rootCmd = &cobra.Command{
 	Short:        "Waits until `done` is issued.",
 	Long:         rootCmdLongDesc,
@@ -16,9 +17,20 @@ var rootCmd = &cobra.Command{
 }
 
 const rootCmdLongDesc = `
-Idle loop to hold a container (or POD) running while some other action happens in the background. It
-is started by issuing "waiter start " and can be stopped with "waiter done", or after timeout. Please
-check "--help" to inspect the flags.
+Idle loop to hold a container (or maybe a Kubernetes POD) running while some other action happens in
+the background. It is started by issuing "waiter start" and can be stopped with "waiter done", or
+after timeout. Please check "--help" to inspect the flags.
+
+	## starts the waiting, use flags to tweak the timeout
+	$ waiter start
+
+	## when done, the lock-file must be removed
+	$ waiter done
+	## (or)
+	$ rm -f <lock-file-path>
+
+In the case of timeout, the waiter will return error, it only exists gracefully via "waiter done", or
+the removal of the lock-file before timeout.
 `
 
 var (
